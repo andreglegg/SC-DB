d8adfd309081849b4ff97247e34b6002c4a2ec40
andreglegg <andreglegg@me.com>
2018-04-27 14:55:25 +0200

    Started working on displaying data via react, having trouble mapping data. will try jsonpath now.

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'whatwg-fetch';
 
 class Home extends Component {
@@ -6,7 +6,8 @@ class Home extends Component {
     super(props);
 
     this.state = {
-      counters: []
+      counters: [],
+      locations: []
     };
 
     this.newCounter = this.newCounter.bind(this);
@@ -25,10 +26,17 @@ class Home extends Component {
           counters: json
         });
       });
+    fetch('/api/location')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          locations: json
+        });
+      });
   }
 
   newCounter() {
-    fetch('/api/counters', { method: 'POST' })
+    fetch('/api/counters', {method: 'POST'})
       .then(res => res.json())
       .then(json => {
         let data = this.state.counters;
@@ -43,7 +51,7 @@ class Home extends Component {
   incrementCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}/increment`, { method: 'PUT' })
+    fetch(`/api/counters/${id}/increment`, {method: 'PUT'})
       .then(res => res.json())
       .then(json => {
         this._modifyCounter(index, json);
@@ -53,7 +61,7 @@ class Home extends Component {
   decrementCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}/decrement`, { method: 'PUT' })
+    fetch(`/api/counters/${id}/decrement`, {method: 'PUT'})
       .then(res => res.json())
       .then(json => {
         this._modifyCounter(index, json);
@@ -63,7 +71,7 @@ class Home extends Component {
   deleteCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}`, { method: 'DELETE' })
+    fetch(`/api/counters/${id}`, {method: 'DELETE'})
       .then(_ => {
         this._modifyCounter(index, null);
       });
@@ -89,20 +97,47 @@ class Home extends Component {
         <p>Counters:</p>
 
         <ul>
-          { this.state.counters.map((counter, i) => (
+          {this.state.counters.map((counter, i) => (
             <li key={i}>
               <span>{counter.count} </span>
               <button onClick={() => this.incrementCounter(i)}>+</button>
               <button onClick={() => this.decrementCounter(i)}>-</button>
               <button onClick={() => this.deleteCounter(i)}>x</button>
             </li>
-          )) }
+          ))}
         </ul>
 
         <button onClick={this.newCounter}>New counter</button>
-      </>
-    );
-  }
-}
 
-export default Home;
+        <p>Locations:</p>
+
+        <ul>
+          {this.state.locations.map((location, i) => {
+            console.log(location.Yela);
+            const planet = location.Yela;
+            let zones = Object.keys(location.Yela);
+            console.log(zones);
+            return (
+              zones.map((key, index) => {
+                const zone = key;
+                let trade = "";
+                Object.keys(planet[zone]).map((k, i)=> {
+                  console.log(k)
+                  trade = k;
+                });
+                return (
+                  <li key={index}>
+                    {zone}
+                    {trade}
+                  </li>
+                )
+              })
+            )
+          })}
+            </ul>
+            </>
+            );
+            }
+          }
+
+          export default Home;
